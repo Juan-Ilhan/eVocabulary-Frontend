6e1dbcaeda7499247a0409aed85986a39e2f8b07
Juan-Ilhan <72693148+Juan-Ilhan@users.noreply.github.com>
2021-12-15 13:28:58 +0100

    Update

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Karteikarten from '../views/Karteikarten.vue'
+import Persons from '../views/Persons.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/karteikarten',
+    name: 'Karteikarten',
+    component: Karteikarten
+  },
+  {
+    path: '/persons',
+    name: 'Persons',
+    component: Persons
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
+  routes
+})
+
+export default router
---
+++ src/views/Karteikarten.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Deine Karteikarten</h1>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Home</h5>
+      <p class="card-text">Haus</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Karteikarten'
+}
+</script>
+
+<style scoped>
+
+</style>
