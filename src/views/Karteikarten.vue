3c125b25812e339fa6d4c89b7ce49d90e436ed1d
Juan-Ilhan <72693148+Juan-Ilhan@users.noreply.github.com>
2021-12-22 09:35:45 +0100

    Karteikarten.vue

--- src/views/Karteikarten.vue
+++ src/views/Karteikarten.vue
@@ -1,32 +1,39 @@
 <template>
-<h1>Deine Karteikarten</h1>
-  <div class="card" style="width: 18rem;">
-    <div class="card" v-for="Karteikarten in Karteikarten" ;key='card.id'>
-          <div class="card-body">
-            <h5 class="card-title">englishWord</h5>
-            <p class="card-text">germanWord</p>
-          </div>
+  <h1>Deine Karteikarten</h1>
+  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">House</div>
+    <div class="card-body">
+      <h5 class="card-title">Haus</h5>
+      <p class="card-text">Gebäude, das Menschen zum Wohnen dient.</p>
     </div>
-   </template>
-
-
+ </div>
+  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">Chair</div>
+    <div class="card-body">
+      <h5 class="card-title">Stuhl</h5>
+      <p class="card-text">Mit vier Beinen, einer Rückenlehne und gelegentlich Armlehnen versehenes Sitzmöbel für eine Person.</p>
+    </div>
+  </div>
+</template>
 <script>
 export default {
   name: 'Karteikarten',
   data () {
     return {
-      cards: {
-    {
-      id: 1,
-        englishWord: 'House',
-      germanWord: 'Haus',
-    }
-      }
+      cards: [
+        {
+          id: 1,
+          englishWord: 'House',
+          germanWord: 'Haus'
+        },
+        {
+          id: 2,
+          englishWord: 'Stuhl',
+          germanWord: 'Chair'
+        }
+      ]
     }
   }
-
-
-
 }
 </script>
 
